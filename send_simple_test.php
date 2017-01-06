60e32fc76d2e7e616786e28a2f3451b3a1c2cb85
Meow <Meow>
2017-01-06 17:24:34 +0800

    check ip

--- send_simple_test.php
+++ send_simple_test.php
@@ -1,4 +1,9 @@
 <?php
+
+error_reporting(-1);
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+
 require_once("phpmailer/class.phpmailer.php");
 //---Send Mail
 	$mail = new PHPMailer();
